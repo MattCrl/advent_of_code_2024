fdcf5f9d804341c573b90bed288d121a0a8751dd
MattCrl <carlevaris@keensaas.fr>
2024-12-02 09:49:26 +0100

    day 2 part 2

--- day_02/day_02.rb
+++ day_02/day_02.rb
@@ -23,4 +23,34 @@ end
 
 puts solve_part_one(read_input_file("./day_02/input.txt"))
 
+def create_sub_arrays(initial_report)
+  save_initial_report = initial_report
+  sub_reports = initial_report.each_index.map do |index|
+    initial_report.reject.with_index { |_, i| i == index }
+  end
+  sub_reports
+end
+
+def solve_part_two(inputs)
+  valid_count = 0
+  sub_valid = false
+  inputs.each do |report|
+    report = report.split.map(&:to_i) 
+    if diff_acceptable?(report, 1, 3) && is_sorted?(report)
+      valid_count += 1
+    else
+      sub_reports = create_sub_arrays(report)
+      sub_reports.each do |sub_report|
+        if diff_acceptable?(sub_report, 1, 3) && is_sorted?(sub_report)
+          sub_valid = true
+        end
+      end
+      valid_count += 1 if sub_valid
+      sub_valid = false
+    end
+  end
+  valid_count
+end
+
+puts solve_part_two(read_input_file("./day_02/input.txt"))
 
