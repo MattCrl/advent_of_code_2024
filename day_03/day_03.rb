214b9fd10cff100a7bfa33272cef2f096b5fc831
MattCrl <mattblogdev@gmail.com>
2024-12-03 17:08:53 +0100

    day 3 part 2

--- day_03/day_03.rb
+++ day_03/day_03.rb
@@ -13,3 +13,24 @@ def solve_part_one(inputs)
 end
 
 puts solve_part_one(read_input_file("./day_03/input.txt"))
+
+def split_with_separators(string_in)
+  string_in.split(/(?=(do\(\)|don't\(\)))/).reject(&:empty?)
+end
+
+def solve_part_two(inputs)
+  total = 0
+  do_dont_splitting = split_with_separators(inputs)
+  do_dont_splitting.each do |instruction|
+    unless instruction.start_with?("don't()")
+      correct_instructions = instruction.scan(/mul\([0-9]{1,3},[0-9]{1,3}\)/)
+      correct_instructions.each do |instruction|
+        numbers = instruction.scan(/[0-9]{1,3}/).map &:to_i
+        total += (numbers.first * numbers.last)
+      end
+    end
+  end
+  total
+end
+
+puts solve_part_two(read_input_file("./day_03/input.txt"))
