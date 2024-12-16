2e7c9316d3b18b581e5ba7b79d0d58c4eeda13c5
MattCrl <mattblogdev@gmail.com>
2024-12-16 13:34:36 +0100

    day 7 part 1

---
+++ day_07/day_07.rb
@@ -0,0 +1,47 @@
+class CombinationCalculator
+  attr_reader :array, :operators
+
+  def initialize(array, operators)
+    @array = array
+    @operators = operators
+  end
+
+  def calculate_combinations
+    operator_combinations.map { |ops| evaluate_combination(ops) }
+  end
+
+  private
+
+  def operator_combinations
+    @operators.repeated_permutation(@array.size - 1).to_a
+  end
+
+  def evaluate_combination(ops)
+    result = @array.first
+    ops.each_with_index do |op, index|
+      result = result.send(op, @array[index + 1])
+    end
+    result
+  end
+end
+
+def read_input_file(input_file)
+  File.read(input_file).split(/\n/)
+end
+
+def solve_part_one(input)
+  total = 0
+  input.each do |line|
+    a = line.split(": ")
+    test_value = a[0].to_i
+    numbers = a[1].split.map &:to_i
+    calculator = CombinationCalculator.new(numbers, [:+, :*])
+    results = calculator.calculate_combinations
+    if results.include? test_value
+      total += test_value
+    end
+  end
+  total
+end
+
+puts solve_part_one(read_input_file("./day_07/input.txt"))
